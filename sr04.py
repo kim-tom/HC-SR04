4e4ec30a0c479f584fbe07127032b5e078a4550a
kim-tom <art.detonation@gmail.com>
2021-04-26 20:54:53 +0900

    initial commit

---
+++ sr04.py
@@ -0,0 +1,41 @@
+from wsgiref.simple_server import make_server
+import RPi.GPIO as GPIO
+import time
+import json
+
+TRIG = 3
+ECHO = 4
+LED = 17
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+GPIO.setup(LED, GPIO.OUT)
+
+def get_distance():
+  GPIO.output(TRIG, GPIO.LOW)
+  time.sleep(0.3)
+  GPIO.output(LED, GPIO.HIGH)
+  GPIO.output(TRIG, GPIO.HIGH)
+  time.sleep(0.00001)
+  GPIO.output(TRIG, GPIO.LOW)
+  while GPIO.input(ECHO) == 0:
+    signaloff = time.time()
+  while GPIO.input(ECHO) == 1:
+    signalon = time.time()
+  GPIO.output(LED, GPIO.LOW)
+  timepassed = signalon - signaloff
+  distance = timepassed * 17000
+  return distance
+def app(environ, start_response):
+  status = '200 OK'
+  headers = [
+    ('Content-type', 'application/json; charset=utf-8'),
+    ('Access-Control-Allow-Origin', '*'),
+  ]
+  start_response(status, headers)
+  return [json.dumps({'distance': get_distance()}).encode("utf-8")]
+
+with make_server('', 3003, app) as httpd:
+  print("Serving on port 3003...")
+  httpd.serve_forever()
